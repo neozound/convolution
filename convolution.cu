486ba932d80057f0cc8d4d8e80e9234e93ae68f4
neozound <rcastano@utp.edu.co>
2018-05-04 13:33:53 -0500

    conv1D sirve sin numeros aleatorios

--- convolution.cu
+++ convolution.cu
@@ -4,16 +4,31 @@
 
 
 void cpu_Conv1D(float *a, float *b, float *c, int size){
-	//If index less than 0 or more or equal than N, set 0 as the array element
-	if (index < 0){
+	int temp = 0;
+        //If index less than 0 or more or equal than N, set 0 as the array element
+	for (int i = 0; i < size; i++){
+	    for (int j = -1; j < 2; j++){
+                if ( ( i+j > 0 ) || ( i+j < size ) ){
+                    temp += a[i+j]*b[i+j];
+                }
+            }
+            c[i] = temp;
+            temp = 0;  
 	}
 }
 
 
-void llenarArray(float * m, int x, float v){
-	for (int i = 0; i < x; i++){
+void fillArray(float * m, int size, float v){
+	for (int i = 0; i < size; i++){
 		m[i] = v;
 	}
+}
+
+void printArray(float *m, int size){
+    for (int i = 0; i < size; i++){
+        printf("%f ", m[i]);
+    }
+    printf("\n");
 }
 
 int main(int argc, char *argv[]){
@@ -23,28 +38,42 @@ int main(int argc, char *argv[]){
 	//set a, b and c arrays
 	float *a, *b, *c;
 	
-	if (argc != 2) {
-		size = atoi(argv[1]);
-		
-		a = (float *) malloc(size*sizeof(float));
-		b = (float *) malloc(size*sizeof(float));
-		c = (float *) malloc(size*sizeof(float));
-		
-		
-		/*
-		Solo hacer multiplicaciones de elementos
-		Que no se encuentren dentro de los límites 
-		de los arreglos
-		*/
+	size = atoi(argv[1]);
+        printf("size: %d\n", size);		
+
+	a = (float *) malloc(size*sizeof(float));
+	b = (float *) malloc(size*sizeof(float));
+	c = (float *) malloc(size*sizeof(float));
 		
 		
-		//llenar arreglo
-		llenarArray(a, size, 2);
-		llenarArray(b, size, 1);
-		llenarArray(c, size, 0);
+	/*
+	Solo hacer multiplicaciones de elementos
+	Que no se encuentren dentro de los límites 
+	de los arreglos
+	*/
 		
-	    cpu_Conv1D(a,b,c,size);
-	}
+		
+	//llenar arreglo
+	fillArray(a, size, 2);
+	fillArray(b, size, 1);
+	fillArray(c, size, 0);
+	
+        printf("a: ");
+        printArray(a, size);
+        printf("b: ");
+        printArray(b, size);
+        printf("c: ");
+        printArray(c, size);
+	
+	cpu_Conv1D(a,b,c,size);
+        
+        printf("New c: ");
+        printArray(c, size);        
+        
+        free(a);
+        free(b);
+        free(c);
+	
 	
 	return 0;
 }
